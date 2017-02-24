9ee8ec5672170a167a35b3819320dc4f141c7bfd
YSY <313008602@qq.com>
2017-02-24 10:39:35 +0800

    自定义view

--- app/src/main/java/myaddasdasd/administrator/example/com/my90/MainActivity.java
+++ app/src/main/java/myaddasdasd/administrator/example/com/my90/MainActivity.java
@@ -1,12 +1,12 @@
 package myaddasdasd.administrator.example.com.my90;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
