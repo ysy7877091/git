0233b324df7be3ef2bbfb0dbf68230cc34af7a69
YSY <313008602@qq.com>
2017-02-24 13:44:41 +0800

    自定义view2

--- app/src/main/java/myaddasdasd/administrator/example/com/my90/MainActivity.java
+++ app/src/main/java/myaddasdasd/administrator/example/com/my90/MainActivity.java
@@ -1,13 +1,28 @@
 package myaddasdasd.administrator.example.com.my90;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToggleButton tb = (ToggleButton)findViewById(R.id.tb);
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(),"kai",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"guan",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
